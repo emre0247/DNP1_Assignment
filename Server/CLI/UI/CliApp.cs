4e236ceade21b85f9e28785c1f016505b46a5037
Emre <147695645+emre0247@users.noreply.github.com>
2024-09-17 17:55:19 +0200

    Added more UI views --> comment and its logic in the switch

--- Server/CLI/UI/CliApp.cs
+++ Server/CLI/UI/CliApp.cs
@@ -1,5 +1,7 @@
+using CLI.UI.ManageComments;
 using CLI.UI.ManagePosts;
 using CLI.UI.ManageUsers;
+using Entities;
 using RepositoryContracts;
 
 namespace CLI.UI;
@@ -19,6 +21,10 @@ public class CliApp
     private readonly ListUsersView listUsersView;
     private readonly ManageUsersView manageUsersView;
     
+    private readonly CreateCommentView createCommentView;
+    private readonly ListCommentsView listCommentsView;
+    private readonly ManageCommentsView manageCommentsView;
+    
     public CliApp(IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
     {
         this.userRepository = userRepository;
@@ -34,6 +40,9 @@ public class CliApp
         listUsersView = new ListUsersView(this.userRepository);
         manageUsersView = new ManageUsersView(this.userRepository);
 
+        createCommentView = new CreateCommentView(this.commentRepository);
+        manageCommentsView = new ManageCommentsView(this.commentRepository);
+        listCommentsView = new ListCommentsView(this.commentRepository);
     }
 
     public async Task StartAsync()
@@ -49,7 +58,10 @@ public class CliApp
             Console.WriteLine("4. View Users");
             Console.WriteLine("5. View Posts");
             Console.WriteLine("6. View Comments");
-            Console.WriteLine("7. Exit");
+            Console.WriteLine("7. Update Post");
+            Console.WriteLine("8. Update Comment");
+            Console.WriteLine("9. Update User");
+            Console.WriteLine("10. Exit");
             
             string option = Console.ReadLine();
 
@@ -62,7 +74,72 @@ public class CliApp
                     Console.WriteLine("Enter Password: ");
                     string password = Console.ReadLine();
                     createUserView.AddUserAsync(userName, password);
-                    break;}
+                    break;
+                
+                case "2":
+                    Console.WriteLine("Enter Post Title: ");
+                    string postTitle = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter Post Content: ");
+                    string postContent = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter User Id");
+                    int userId = int.Parse(Console.ReadLine());
+                    
+                    createPostView.AddPostAsync(postTitle, postContent, userId);
+                    break;
+                
+                case "3":
+                    Console.WriteLine("Enter Comment Content: ");
+                    string commentContent = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter Post Id: ");
+                    int postId = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("Enter User Id: ");
+                    int userID = int.Parse(Console.ReadLine());
+                    
+                    createCommentView.AddCommentAsync(commentContent, postId, userID);
+                    break;
+                
+                case "4":
+                    listUsersView.ListUsers();
+                    break;
+                
+                case "5":
+                    listPostsView.ListPosts();
+                    break;
+                
+                case "6":
+                    listCommentsView.ListComments();
+                    break;
+                
+                case "7": 
+                    Console.WriteLine("Enter Post Title: ");
+                    string title = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter Post Content: ");
+                    string postContent1 = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter Post Id: ");
+                    int postIds = int.Parse(Console.ReadLine());
+                    await managePostsView.UpdatePostAsync(postIds, title, postContent1 );
+                    break;
+                
+                case "8":
+                    Console.WriteLine("Enter Comment Body: ");
+                    string s1 = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter Comment Id: ");
+                    int commentId = int.Parse(Console.ReadLine());
+
+                    await manageCommentsView.UpdateCommentAsync(s1, commentId);
+                    break;
+                
+                case "9":
+                    
+                    break;
+            }
         }
         Console.WriteLine("");
     }
