cb056213eb506c695ff065954aad3c7e73f6d4b7
Emre <147695645+emre0247@users.noreply.github.com>
2024-09-25 19:47:46 +0200

    Adding logic to the file repository now Add and Update finished

---
+++ Server/FileRepositories/CommentFileRepository.cs
@@ -0,0 +1,63 @@
+using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+using InvalidOperationException = System.InvalidOperationException;
+
+namespace FileRepositories;
+
+public class CommentFileRepository : ICommentRepository
+{
+    private readonly string filePath = "comments.json";
+
+
+    public CommentFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+    public async Task<Comment> AddAsync(Comment comment)
+    {
+        string commentAsJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentAsJson)!;
+        int maxId = comments.Count > 0 ? comments.Max(x => x.Id) : 1;
+        comment.Id = maxId + 1;
+        comments.Add(comment);
+        commentAsJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentAsJson);
+        return comment;
+    }
+
+    public async Task UpdateAsync(Comment comment)
+    {
+        string commentAsJson = await File.ReadAllTextAsync(filePath);
+        List<Comment> comments = JsonSerializer.Deserialize<List<Comment>>(commentAsJson)!;
+        Comment? existingComment = comments.SingleOrDefault(p => p.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{comment.Id}' not found");
+        }
+
+        comments.Remove(existingComment);
+        comments.Add(existingComment);
+        commentAsJson = JsonSerializer.Serialize(comments);
+        await File.WriteAllTextAsync(filePath, commentAsJson);
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        throw new NotImplementedException();
+    }
+}
