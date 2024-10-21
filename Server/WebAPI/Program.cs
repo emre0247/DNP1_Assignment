79b9519835b0267cbb22dba1a81e3850b55a170b
Emre <147695645+emre0247@users.noreply.github.com>
2024-10-21 18:35:23 +0200

    Register repository implementation

--- Server/WebAPI/Program.cs
+++ Server/WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using FileRepositories;
+using RepositoryContracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPostRepository, PostFileRepository>();
+builder.Services.AddScoped<IUserRepository, UserFileRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentFileRepository>();
 
 var app = builder.Build();
 
