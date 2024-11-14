70060abc3857aef1cd8744aa7423e2dc46be8694
Emre <147695645+emre0247@users.noreply.github.com>
2024-11-14 19:14:13 +0100

    Login Method and logout method

--- BlazorApp/Auth/SimpleAuthProvider.cs
+++ BlazorApp/Auth/SimpleAuthProvider.cs
@@ -1,11 +1,59 @@
+using System.Security.Claims;
+using System.Text.Json;
+using DTOs;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorApp.Auth;
 
 public class SimpleAuthProvider : AuthenticationStateProvider
 {
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    private readonly HttpClient httpClient;
+    private ClaimsPrincipal currentClaimsPrincipal;
+
+    public SimpleAuthProvider(HttpClient httpClient)
     {
-        throw new NotImplementedException();
+        this.httpClient = httpClient;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        return new AuthenticationState(currentClaimsPrincipal ?? new ());
+    }
+    
+    public void Logout()
+    {
+        currentClaimsPrincipal = new();
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(currentClaimsPrincipal)));
+    }
+
+    public async Task Login(string userName, string password)
+    {
+        HttpResponseMessage response = await httpClient.PostAsJsonAsync(
+            "auth/login",
+            new LoginRequest(userName, password));
+
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        UserDTO userDto = JsonSerializer.Deserialize<UserDTO>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+
+
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.Name, userDto.Username),
+            new Claim("Id", userDto.Id.ToString()),
+        };
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "apiauth");
+        currentClaimsPrincipal = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(
+            Task.FromResult(new AuthenticationState(currentClaimsPrincipal))
+        );
     }
 }
